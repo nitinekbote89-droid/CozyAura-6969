21b5462380116c4537837c6e33d65df030690d0d
nitinekbote89-droid <nitinekbote89@gmail.com>
2026-07-12 18:35:03 +0530

    chore: Configure Wrangler and Cloudflare Pages setup with hybrid astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
+import cloudflare from "@astrojs/cloudflare";
+
+const adapter = process.env.CF_PAGES === '1' || process.env.CLOUDFLARE === 'true'
+  ? cloudflare()
+  : netlify();
 
 export default defineConfig({
   output: 'server',
   prefetch: false,
-  adapter: netlify(),
+  adapter: adapter,
   vite: {
     build: {
       modulePreload: {

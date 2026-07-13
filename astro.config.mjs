c6a485f84ae8bd544f09774e5e617ad361194f9b
nitinekbote89-droid <nitinekbote89@gmail.com>
2026-07-13 14:30:16 +0530

    feat: Integrate automated sitemap generation and prerender admin dashboard

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import cloudflare from "@astrojs/cloudflare";
+import sitemap from '@astrojs/sitemap';
 
 const adapter = process.env.NETLIFY === 'true'
   ? netlify()
   : cloudflare();
 
 export default defineConfig({
+  site: 'https://cozyaura-6969.nitinekbote89.workers.dev',
   output: 'server',
   prefetch: false,
   adapter: adapter,
+  integrations: [sitemap()],
   vite: {
     build: {
       modulePreload: {
